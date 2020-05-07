249d352630cbaf773b45fe904c37a7e8f19fcfe2
Mario Garzon <marioney@gmail.com>
2020-05-07 11:29:49 +0200

    Add activate observer script

---
+++ scripts/activate_observer.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import rospy
+
+from yaml import load
+# import roslib
+# roslib.load_manifest("rosparam")
+# import rosparam
+from rospkg import RosPack
+from controller_manager_msgs.srv import LoadController, LoadControllerRequest
+
+if __name__ == '__main__':
+    rospy.init_node('activate_observer_node')
+    rospack = RosPack()
+
+    # param = rosparam.load_file(rospack.get_path('metacontrol_sim')+'/yaml/goal.yaml')
+    observer_dict = load(file(rospack.get_path(
+        'metacontrol_sim')+'/yaml/observers.yaml', 'r'))
+
+    observer_req = LoadControllerRequest()
+    observer_req.name = observer_dict['name']
+
+    # Wait for the observer service to become available.
+    rospy.loginfo("Waiting for service...")
+    rospy.wait_for_service('/load_observer')
+    try:
+        # Create a service proxy.
+        observer_srv_proxy = rospy.ServiceProxy('/load_observer', LoadController)
+
+        # Call the service here.
+        service_response = observer_srv_proxy(observer_req)
+
+        print("The load observer service call is completed!")
+    except rospy.ServiceException, e:
+        print "load observer Service call failed: %s"%e
+
+    rospy.loginfo('Safe shutdown')
