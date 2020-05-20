8094ac7c23cdab5fc4d2ed9e924f35fe3091ad8c
Mario Garzon <marioney@gmail.com>
2020-05-20 19:37:22 +0200

    Update activate multiple observers by reading them from yaml file

--- scripts/activate_observer.py
+++ scripts/activate_observer.py
@@ -16,22 +16,21 @@ if __name__ == '__main__':
     # param = rosparam.load_file(rospack.get_path('metacontrol_sim')+'/yaml/goal.yaml')
     observer_dict = load(file(rospack.get_path(
         'metacontrol_sim')+'/yaml/observers.yaml', 'r'))
-
-    observer_req = LoadControllerRequest()
-    observer_req.name = observer_dict['name']
-
-    # Wait for the observer service to become available.
-    rospy.loginfo("Waiting for service...")
-    rospy.wait_for_service('/load_observer')
-    try:
-        # Create a service proxy.
-        observer_srv_proxy = rospy.ServiceProxy('/load_observer', LoadController)
-
-        # Call the service here.
-        service_response = observer_srv_proxy(observer_req)
-
-        print("The load observer service call is completed!")
-    except rospy.ServiceException, e:
-        print "load observer Service call failed: %s"%e
+    for observer_entry in observer_dict:
+        print observer_entry
+        observer_req = LoadControllerRequest()
+        observer_req.name = observer_dict[observer_entry]['name']
+        print observer_req.name 
+        # Wait for the observer service to become available.
+        rospy.loginfo("Waiting for service...")
+        rospy.wait_for_service('/load_observer')
+        try:
+            # Create a service proxy.
+            observer_srv_proxy = rospy.ServiceProxy('/load_observer', LoadController)
+            # Call the service here.
+            service_response = observer_srv_proxy(observer_req)
+            print("The load observer service call is completed!")
+        except rospy.ServiceException, e:
+            print "load observer Service call failed: %s"%e
 
     rospy.loginfo('Safe shutdown')
